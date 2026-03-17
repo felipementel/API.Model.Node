09203ed5faeacdb5e03aa1eb15571f1f8da12f80
Felipe Augusto <felipementel@hotmail.com>
2026-03-17 08:07:09 -0300

    Change app host to listen on all interfaces

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ async function start(): Promise<void> {
   const app = await createApp();
 
   try {
-    await app.listen({ host: '127.0.0.1', port: 8000 });
+    await app.listen({ host: '0.0.0.0', port: 8000 });
   } catch (error) {
     app.log.error(error);
     process.exit(1);
